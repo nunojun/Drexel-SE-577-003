59e5e234c3a62cda81e76dd00d795a5d46e41491
Bumjoon Kim <nunojun@gmail.com>
2022-04-17 20:32:46 -0600

    Added src by copying from ArchitectingSoftware/se577-web-demo.

---
+++ src/router/routes.ts
@@ -0,0 +1,33 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/AppPages/Home.vue') },
+      { path: '/about', component: () => import('pages/AppPages/About.vue') },
+      {
+        path: '/about2',
+        component: () => import('pages/AppPages/AboutModular.vue'),
+      },
+      {
+        path: '/dynamic',
+        component: () => import('pages/AppPages/Dynamic.vue'),
+      },
+      {
+        path: '/props',
+        component: () => import('pages/AppPages/PropsPage.vue'),
+      },
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;

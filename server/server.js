69d441b5ead92a8c284752165214d6d34a9f5946
Bumjoon Kim <nunojun@gmail.com>
2022-05-22 08:07:46 -0600

    Customized server.js to support docker.

--- server/server.js
+++ server/server.js
@@ -65,7 +65,7 @@ fastify.get('/gh_db', async (req, reply) => {
 
 const start = async() => {
   try {
-    await fastify.listen(PORT)
+    await fastify.listen(PORT, '0.0.0.0')
   } catch (error) {
     fastify.log.error(error)
     process.exit(1)
